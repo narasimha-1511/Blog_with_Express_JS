c430420e80d992d8072a0cb6f2ef91a2834f836f
VinayReddy18 <142021280+VinnuReddy18@users.noreply.github.com>
2023-10-22 12:04:12 +0530

    Indentation Update blogs.js

--- data/blogs.js
+++ data/blogs.js
@@ -1,74 +1,54 @@
-blogs = [
+let blogs = [
   {
     title: "How to get started with python",
-    content: `Getting started with Python is relatively straightforward and beginner-friendly. Here's a step-by-step guide to help you begin your journey with Python:
+    content: `Getting started with Python is relatively straightforward and beginner-friendly. Here's a step-by-step guide to help you begin your journey with Python:<br>
 
-1. **Install Python:**
-   - First, you need to install Python on your computer if it's not already installed. Visit the official Python website at [python.org](https://www.python.org/downloads/) to download the latest version of Python. Follow the installation instructions for your operating system.
+<b>1. Install Python:</b><br>
+   - First, you need to install Python on your computer if it's not already installed. Visit the official Python website at python.org to download the latest version of Python. Follow the installation instructions for your operating system.<br>
 
-2. **Choose a Code Editor or IDE:**
-   - You can write Python code in a simple text editor like Notepad (for Windows) or TextEdit (for macOS). However, it's often more convenient to use an Integrated Development Environment (IDE) or code editor specifically designed for Python, such as:
-     - [Visual Studio Code](https://code.visualstudio.com/)
-     - [PyCharm](https://www.jetbrains.com/pycharm/)
-     - [Jupyter Notebook](https://jupyter.org/)
+<b>2. Choose a Code Editor or IDE:</b><br>
+   - You can write Python code in a simple text editor like Notepad (for Windows) or TextEdit (for macOS). However, it's often more convenient to use an Integrated Development Environment (IDE) or code editor specifically designed for Python, such as:<br>
+     - Visual Studio Code<br>
+     - PyCharm<br>
+     - Jupyter Notebook<br>
 
-3. **Write and Run Your First Python Program:**
-   - Open your code editor or IDE, create a new Python file (usually with a .py extension), and write a simple Python program. For example, you can start with a classic "Hello, World!" program:
+<b>3. Write and Run Your First Python Program:</b><br>
+   - Open your code editor or IDE, create a new Python file (usually with a .py extension), and write a simple Python program. For example, you can start with a classic "Hello, World!" program:<br>
 
    \`\`\`python
    print("Hello, World!")
-   \`\`\`
+   \`\`\`<br>
 
-4. **Understanding Python Syntax:**
-   - Python uses indentation (whitespace) to define blocks of code. Indentation is crucial, so make sure your code is properly formatted. Study Python's basic syntax rules, such as variables, data types, and common operators.
+<b>4. Understanding Python Syntax:</b><br>
+   - Python uses indentation (whitespace) to define blocks of code. Indentation is crucial, so make sure your code is properly formatted. Study Python's basic syntax rules, such as variables, data types, and common operators.<br>
 
-5. **Learn Python Basics:**
-   - To progress, it's essential to understand fundamental concepts like variables, data types (integers, strings, lists, dictionaries, etc.), conditional statements (if, elif, else), loops (for, while), and functions. You can find tutorials and documentation online to learn these topics.
+<b>5. Learn Python Basics:</b><br>
+   - To progress, it's essential to understand fundamental concepts like variables, data types (integers, strings, lists, dictionaries, etc.), conditional statements (if, elif, else), loops (for, while), and functions. You can find tutorials and documentation online to learn these topics.<br>
 
-6. **Practice and Experiment:**
-   - The best way to learn Python is through practice. Write simple programs, experiment with different Python features, and solve problems. Try online coding platforms like [LeetCode](https://leetcode.com/), [HackerRank](https://www.hackerrank.com/), or [Codecademy](https://www.codecademy.com/) for exercises and projects.
+<b>6. Practice and Experiment:</b><br>
+   - The best way to learn Python is through practice. Write simple programs, experiment with different Python features, and solve problems. Try online coding platforms like LeetCode, HackerRank, or Codecademy for exercises and projects.<br>
 
-7. **Explore Python Libraries and Modules:**
-   - Python has a vast standard library and numerous third-party libraries and modules that you can use for various purposes. Familiarize yourself with some common libraries, like NumPy for scientific computing, pandas for data analysis, and matplotlib for data visualization.
+<b>7. Explore Python Libraries and Modules:</b><br>
+   - Python has a vast standard library and numerous third-party libraries and modules that you can use for various purposes. Familiarize yourself with some common libraries, like NumPy for scientific computing, pandas for data analysis, and matplotlib for data visualization.<br>
 
-8. **Learn Object-Oriented Programming (Optional):**
-   - Python supports object-oriented programming (OOP). Learning OOP concepts can be beneficial as your projects become more complex.
+<b>8. Learn Object-Oriented Programming (Optional):</b><br>
+   - Python supports object-oriented programming (OOP). Learning OOP concepts can be beneficial as your projects become more complex.<br>
 
-9. **Version Control with Git:**
-   - Learning to use Git for version control is important for collaboration and code management. Platforms like GitHub or GitLab can help you host and share your Python projects.
+<b>9. Version Control with Git:</b><br>
+   - Learning to use Git for version control is important for collaboration and code management. Platforms like GitHub or GitLab can help you host and share your Python projects.<br>
 
-10. **Documentation and Online Resources:**
-    - Python has extensive documentation at [docs.python.org](https://docs.python.org/). Additionally, online tutorials, forums, and communities like Stack Overflow can provide valuable support and answers to your questions.
+<b>10. Documentation and Online Resources:</b><br>
+    - Python has extensive documentation at [docs.python.org. Additionally, online tutorials, forums, and communities like Stack Overflow can provide valuable support and answers to your questions.<br>
 
-11. **Expand Your Knowledge:**
-    - As you become more proficient, explore more advanced topics like web development, data science, machine learning, or automation, depending on your interests and goals.
+<b>11. Expand Your Knowledge:</b><br>
+    - As you become more proficient, explore more advanced topics like web development, data science, machine learning, or automation, depending on your interests and goals.<br>
 
-12. **Keep Practicing:**
-    - Python is a skill that improves with practice. The more you code and work on projects, the better you'll become.
+<b>12. Keep Practicing:</b><br>
+    - Python is a skill that improves with practice. The more you code and work on projects, the better you'll become.<br>
 
 Remember that learning any programming language takes time and patience. Don't be discouraged by initial challenges, and keep experimenting and building to enhance your Python skills.`,
     slug: "python",
-  },
-  {
-    title: "How to get started with JS",
-    content: "This is Content JS",
-    slug: "JS",
-  },
-  {
-    title: "How to get started with webdev",
-    content: "This is Content WebDev",
-    slug: "WebDev",
-  },
-  {
-    title: "How to get started with CSS",
-    content: "This is Content CSS",
-    slug: "CSS",
-  },
-  {
-    title: "How to get started with Javasc",
-    content: "This is Javasc",
-    slug: "Javasc",
-  },
+  }
 ];
 
 module.exports = blogs;
